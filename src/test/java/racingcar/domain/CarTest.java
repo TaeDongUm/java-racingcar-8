f170c5d61be0b983caee81e2e9a2f1335c79d37b
etd93 <etd93@naver.com>
2025-10-27 00:52:34 +0900

    test(CarTest): 처음 생성된 자동차의 시작 위치는 0이다 테스트 작성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("처음 생성된 자동차의 시작 위치는 0이어야 한다.")
+    void Car_Start_Position_Is_Zero() {
+
+        Car car = new Car("won");
+        assertThat(car.position).isEqualTo(0);
+    }
+}
