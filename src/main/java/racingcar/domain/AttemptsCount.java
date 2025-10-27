09e448a06606587ed08d58e0450b027ec8aff81b
etd93 <etd93@naver.com>
2025-10-27 17:50:36 +0900

    feat(AttemptsCount): 사용자 이동 횟수 입력값 처리 구현

---
+++ src/main/java/racingcar/domain/AttemptsCount.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+public final class AttemptsCount {
+    private final int value;
+
+    private AttemptsCount(int value) {
+        this.value = value;
+    }
+
+    public static AttemptsCount of(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("ERROR: 이동 횟수는 1 이상이어야 합니다.");
+        }
+        return new AttemptsCount(value);
+    }
+
+    public int value() {
+        return value;
+    }
+}
