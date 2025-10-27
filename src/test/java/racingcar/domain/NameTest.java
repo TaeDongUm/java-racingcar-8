d57af89bfb5b40e595ba0df575cb80a55148fc73
etd93 <etd93@naver.com>
2025-10-27 17:00:28 +0900

    test(NameTest):  빈 문자열이면 예외가 발생한다 및 이름에 공백이 존재하면 예외가 발생한다 테스트 추가

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -20,4 +20,35 @@ class NameTest {
         assertThat(Name.of("a")).isNotNull();        // 1자
         assertThat(Name.of("abcde")).isNotNull();    // 5자 경계
     }
+
+    @DisplayName("이름이 빈 문자열이면 예외가 발생한다")
+    @Test
+    void empty_Is_Invalid() {
+        assertThatThrownBy(() -> Name.of(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이름이 공백만으로 이루어지면 예외가 발생한다")
+    @Test
+    void blank_Only_Is_Invalid() {
+        assertThatThrownBy(() -> Name.of("   "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이름에 공백 문자가 하나라도 포함되면 예외가 발생한다(내부/앞/뒤 포함)")
+    @Test
+    void any_Whitespace_Is_Invalid() {
+        assertThatThrownBy(() -> Name.of("po bi"))   // 내부 공백
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Name.of(" pobi"))   // 앞 공백
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Name.of("pobi "))   // 뒤 공백
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Name.of("po\tbi"))  // 탭
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Name.of("po\nbi"))  // 개행
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
