e2d1ab0a31e32da45e7708d4a3520522da04d907
etd93 <etd93@naver.com>
2025-10-27 21:30:01 +0900

    feat: 프로그램 컨트롤러 구현

---
+++ src/main/java/racingcar/controller/RaceGameController.java
@@ -0,0 +1,47 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.MoveStrategy;
+import racingcar.domain.RandomNumberPicker;
+import racingcar.domain.ThresholdMoveStrategy;
+import racingcar.domain.WinnerSelection;
+import racingcar.dto.CarGameState;
+import racingcar.service.RacingGame;
+import racingcar.view.AttemptsParser;
+import racingcar.view.InputView;
+import racingcar.view.NameParser;
+import racingcar.view.OutputView;
+
+public class RaceGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RaceGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<String> carNames = NameParser.parseNames(inputView.inputCarName());
+        int attempts = AttemptsParser.parseAttempts(inputView.inputAttempts());
+
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        MoveStrategy moveStrategy = new ThresholdMoveStrategy(new RandomNumberPicker());
+        RacingGame racingGame = new RacingGame(cars, moveStrategy);
+
+        outputView.printExecutionResultHeader();
+        racingGame.raceStart(attempts);
+
+        List<List<CarGameState>> gameHistory = racingGame.snapshots();
+        gameHistory.forEach(outputView::printRoundResult);
+
+        List<String> winners = WinnerSelection.selectByMaxPosition(gameHistory);
+        outputView.printWinners(winners);
+    }
+}
