57cefc728746c8747c4615f404cd486c9d6c7daf
etd93 <etd93@naver.com>
2025-10-27 17:21:07 +0900

    feat(Name):  빈 문자열이면 예외가 발생한다 및 이름에 공백이 존재하면 예외가 발생한다 기능 구현

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -13,6 +13,17 @@ public final class Name {
 
     public static Name of(String raw) {
         Objects.requireNonNull(raw, "ERROR: 자동차 이름은 NULL일 수 없습니다.");
+
+        if (raw.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: 이름은 빈 문자열이면 안됩니다.");
+        }
+
+        for (int i = 0; i < raw.length(); i++) {
+            if (Character.isWhitespace(raw.charAt(i))) {
+                throw new IllegalArgumentException("ERROR: 공백이 포함되면 안됩니다.");
+            }
+        }
+
         if (raw.length() > MAX_LEN) {
             throw new IllegalArgumentException("ERROR: 자동차 이름은 5글자 이하여야 합니다.");
         }
