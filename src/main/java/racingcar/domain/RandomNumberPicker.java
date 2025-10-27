4268613463631488fbc409cc51d020d184930c3a
etd93 <etd93@naver.com>
2025-10-27 21:29:19 +0900

    feat: 랜덤값 생성 기능 구현

---
+++ src/main/java/racingcar/domain/RandomNumberPicker.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberPicker implements NumberPicker {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
+    @Override
+    public int pick() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
